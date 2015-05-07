9148d739fa7e45ed27fa01dbf9f1dfb8520886b3
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2015-05-07 16:57:46 +0200

    Simplifying the update animation in  the Forecast Adapter. Deleting the Sparse<Animation> and sparse<Runnable> and putting the animation and its runnable in its ViewHolder

--- ForecastYahooRest/src/com/android2ee/formation/restservice/sax/forecastyahoo/view/forecast/arrayadpater/ForecastArrayAdapter.java
+++ ForecastYahooRest/src/com/android2ee/formation/restservice/sax/forecastyahoo/view/forecast/arrayadpater/ForecastArrayAdapter.java
@@ -35,7 +35,6 @@ import android.animation.AnimatorSet;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Handler;
-import android.support.v4.util.SparseArrayCompat;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.util.SparseBooleanArray;
@@ -65,17 +64,6 @@ public class ForecastArrayAdapter extends ArrayAdapter<YahooForcast> {
 	 * Handler to launch the animation runnable
 	 */
 	Handler handlerForAnimation;
-	/**
-	 * An animation by item is needed
-	 * You need one animation by item
-	 * else the animation is relaunch for every item (one animpation for all)
-	 */
-	SparseArrayCompat<Animation> sparseAnimation;
-	/**
-	 * An animation by item is needed
-	 * The same, you need one runnable by item
-	 */
-	SparseArrayCompat<MyRunnable> sparseRunnable;
     /**
      * To know when the item is flipped or not
      * When flipped it show us its back side else its front side
@@ -115,15 +103,8 @@ public class ForecastArrayAdapter extends ArrayAdapter<YahooForcast> {
 		ctx = context;
         postJB = context.getResources().getBoolean(R.bool.postJB);
         postHC = context.getResources().getBoolean(R.bool.postHC);
-		//instantiate the sprase array
-		sparseAnimation=new SparseArrayCompat<Animation>(getCount());
-		sparseRunnable=new SparseArrayCompat<MyRunnable>(getCount());
 		//instantiate the sparse array
 		handlerForAnimation = new Handler();
-		for(int i=0;i<getCount();i++) {
-			sparseAnimation.put(i, AnimationUtils.loadAnimation(context, R.anim.anim_item_updated));
-			sparseRunnable.put(i, new MyRunnable( i));
-		}
         isFlipped=new SparseBooleanArray();
 	}
 
@@ -191,12 +172,7 @@ public class ForecastArrayAdapter extends ArrayAdapter<YahooForcast> {
 		}
 		// launch animations to show the update to the user (not the first time but only when refreshing)
 		if (firstLaunch>=2) {
-			//hide the item
-//			viewHolder.getLinRoot().setVisibility(View.GONE);
-			//find the runnable and set the vierwHolder
-			sparseRunnable.get(position).setVh(viewHolder);
-			//then launch the runnable in 300ms *pos
-			handlerForAnimation.postDelayed(sparseRunnable.get(position), 300 * position);			
+            viewHolder.launchUpdateAnimation();
 		}
         //and finally manage the visibility of the side : front or back side is visible
         manageSideVisibility(position);
@@ -258,49 +234,14 @@ public class ForecastArrayAdapter extends ArrayAdapter<YahooForcast> {
 		 * The viewHolder that contains the view to animate
 		 */
 		private ViewHolder vh;
-		/**
-		 * The position of the item
-		 */
-		private int position;
 
-		public MyRunnable( int pos) {
-			this.position = pos;
-		}
-
-		/**
-		 * @param vh the vh to set
-		 */
-		public final void setVh(ViewHolder vh) {
-			if(this.vh!=null){
-                //it means an already runnable is associated with this item
-                //we need to remove it (else it gonna run the animation twice
-                //and it's strange for the user)
-                handlerForAnimation.removeCallbacks(sparseRunnable.get(position));
-            }
-            this.vh = vh;
+		public MyRunnable(ViewHolder vh) {
+			this.vh=vh;
 		}
 
 		public void run() {
-            //find the animation to launch
-            Animation animationUpdate = sparseAnimation.get(position);
-
-            if(isFlipped.get(position)) {
-                vh.getImvBack().startAnimation(animationUpdate);
-            }else{
-                //first set the linearlayout visible
-                vh.getLinRoot().setVisibility(View.VISIBLE);
-                //run it
-//			vh.getImvIcon().startAnimation(animationUpdate);
-//			vh.getTxvCurrent().startAnimation(animationUpdate);
-//			vh.getTxvDate().startAnimation(animationUpdate);
-//			vh.getTxvMax().startAnimation(animationUpdate);
-//			vh.getTxvMin().startAnimation(animationUpdate);
-//			vh.getTxvTendance().startAnimation(animationUpdate);
-                vh.getLinRoot().startAnimation(animationUpdate);
-            }
+            vh.animateUpdate();
 		}
-
-
 	}
 
 	/******************************************************************************************/
@@ -316,6 +257,9 @@ public class ForecastArrayAdapter extends ArrayAdapter<YahooForcast> {
 		TextView txvCurrent;
 		TextView txvMin;
         TextView txvMax;
+        //For Update animation
+        Animation updateAnimation;
+        MyRunnable animationRunnable;
         //For animation
         ImageView imvBack;
         int currentPosition;
@@ -335,6 +279,8 @@ public class ForecastArrayAdapter extends ArrayAdapter<YahooForcast> {
 		private ViewHolder(View rowview) {
 			super();
 			this.view = rowview;
+            updateAnimation=AnimationUtils.loadAnimation(getContext(), R.anim.anim_item_updated);
+            animationRunnable=new MyRunnable(this);
 		}
 
 		/**
@@ -434,6 +380,37 @@ public class ForecastArrayAdapter extends ArrayAdapter<YahooForcast> {
 			}
 			return linRoot;
 		}
+        /**************************************************
+         * Animation tricks
+         * All Version
+         * The UpdateAnimation
+         * **************************************************
+         */
+        /**
+         * Launch the Update Animation
+         */
+        public void animateUpdate(){
+            if(isFlipped.get(currentPosition)) {
+                getImvBack().startAnimation(updateAnimation);
+            }else{
+                //first set the linearlayout visible
+                getLinRoot().setVisibility(View.VISIBLE);
+                //run it
+                getLinRoot().startAnimation(updateAnimation);
+            }
+        }
+        /**
+         * Launch the Update Animation
+         */
+        public void launchUpdateAnimation(){
+            //it means an already runnable is associated with this item
+                //we need to remove it (else it gonna run the animation twice
+                //and it's strange for the user)
+            handlerForAnimation.removeCallbacks(animationRunnable);
+            //then launched it in few seconds
+            handlerForAnimation.postDelayed(animationRunnable, 300 * currentPosition);
+        }
+
         /**************************************************
          * Animation tricks
          * postHoneyComb
