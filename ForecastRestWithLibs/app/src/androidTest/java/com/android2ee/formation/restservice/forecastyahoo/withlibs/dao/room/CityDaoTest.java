9ec1e4c870d9eafd0344231548702cbb309aeee8
Marion Aubard <marion.aubard@sylpheo.com>
2018-06-14 11:07:17 +0200

    Add missing tests

--- ForecastRestWithLibs/app/src/androidTest/java/com/android2ee/formation/restservice/forecastyahoo/withlibs/dao/room/CityDaoTest.java
+++ ForecastRestWithLibs/app/src/androidTest/java/com/android2ee/formation/restservice/forecastyahoo/withlibs/dao/room/CityDaoTest.java
@@ -24,6 +24,7 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -42,6 +43,7 @@ public class CityDaoTest {
     private static final String CITY_SAINT_QUENTIN = "Saint-Quentin";
 
     Observer<List<City>> cityObserver;
+    Observer<City> oneCityObserver;
 
     @Before
     public void createDb() {
@@ -88,6 +90,66 @@ public class CityDaoTest {
     }
 
     @Test
+    public void testPopulateAll(){
+        //declare the observer
+        cityObserver = new Observer<List<City>>() {
+            @Override
+            public void onChanged(@Nullable List<City> cities) {
+                if(cities.size() == 0){
+                    //first call
+                } else{
+                    Log.e(TAG,"Toto est un roi");
+                    Assert.assertEquals(2, cities.size());
+                }
+            }
+        };
+
+        //declare the LiveData you listen,
+        LiveData<List<City>> lvCities = db.getCityDao().loadAllLiveData();
+
+        //bind the observer to the live data
+        lvCities.observeForever(cityObserver);
+
+        /**Make you insertion or DB stuff*/
+        //create the City (no city no foreign key, no weatherData)
+        City city1 = DataGeneratorSimple.getCity(CITY_SAINT_QUENTIN);
+        City city2 = DataGeneratorSimple.getCity(CITY_PALAMINY);
+        List<City> cities = Arrays.asList(city1, city2);
+        db.getCityDao().insertAll(cities);
+
+        //Remove observer
+        //it works because you allow the main thread query:allowMainThreadQueries
+        lvCities.removeObserver(cityObserver);
+    }
+
+    @Test
+    public void testLoadLiveDataById(){
+        //declare the observer
+        oneCityObserver = new Observer<City>() {
+            @Override
+            public void onChanged(@Nullable City city) {
+                Assert.assertEquals(CITY_PALAMINY, city.getName());
+            }
+        };
+
+        /**Make you insertion or DB stuff*/
+        //create the City (no city no foreign key, no weatherData)
+        City city = DataGeneratorSimple.getCity("Palaminy");
+        long id = db.getCityDao().insert(city);
+
+        //declare the LiveData you listen,
+        LiveData<City> lvCity = db.getCityDao().loadLiveDataById(id);
+
+        //bind the observer to the live data
+        lvCity.observeForever(oneCityObserver);
+
+
+        //Remove observer
+        //it works because you allow the main thread query:allowMainThreadQueries
+        lvCity.removeObserver(oneCityObserver);
+    }
+
+    @Test
     public void testDelete() {
         //declare the observer
         cityObserver = new Observer<List<City>>() {
@@ -115,6 +177,39 @@ public class CityDaoTest {
         lvCities.observeForever(cityObserver);
 
         db.getCityDao().delete(city);
+
+        //Remove observer
+        //it works because you allow the main thread query:allowMainThreadQueries
+        lvCities.removeObserver(cityObserver);
+    }
+
+    @Test
+    public void testDeleteById() {
+        //declare the observer
+        cityObserver = new Observer<List<City>>() {
+            @Override
+            public void onChanged(@Nullable List<City> cities) {
+                if (cities.size() == 1){
+                    //first call
+                } else if(cities.size() == 0){
+                    Log.e(TAG,"Toto est un roi");
+                    Assert.assertEquals(0, cities.size());
+                }
+            }
+        };
+
+        //declare the LiveData you listen,
+        LiveData<List<City>> lvCities = db.getCityDao().loadAllLiveData();
+
+        /**Make you insertion or DB stuff*/
+        //create the City (no city no foreign key, no weatherData)
+        City city = DataGeneratorSimple.getCity(CITY_PALAMINY);
+        long id = db.getCityDao().insert(city);
+
+        //bind the observer to the live data
+        lvCities.observeForever(cityObserver);
+
+        db.getCityDao().delete(id);
 
         //Remove observer
         //it works because you allow the main thread query:allowMainThreadQueries
